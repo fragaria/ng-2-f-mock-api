b6899cc05d8b07c3341a244e059cd58fe9ebc558
michal dub <michal.dub@fragaria.cz>
2016-10-27 15:16:58 +0200

    updated clientsRelated array

--- src/mock-api/mock-data.ts
+++ src/mock-api/mock-data.ts
@@ -3,8 +3,9 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class MockData implements InMemoryDbService {
   createDb() {
     let clientsRelated = [
-      { id: 13, name: "Karel Vomáčka", personalIdentNumber: "9007260987", email: "karel@michal.cz" },
-      { id: 14, name: "Janek Vomáčka", personalIdentNumber: "9009260987", email: "karel@li.cz" }
+      { id: 13, name: "Karel Vomáčka", personalIdentNumber: "9007260987", email: "karel@michal.cz", relatedWith: 1 },
+      { id: 14, name: "Jan Novák", personalIdentNumber: "9109260987", email: "jan@li.cz", relatedWith: 1 },
+      { id: 15, name: "Janek Vomáčka", personalIdentNumber: "9209260987", email: "karel@li.cz", relatedWith: 2 }
     ];
     let items = [
       { id: 11, title: 'Boots' },
